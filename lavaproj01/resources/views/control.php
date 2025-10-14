075082a637ee7d6e9537c68d5f8273e641539954
StevX119 <stevengt256@gmail.com>
2025-10-14 12:09:50 +0800

    update 4

---
+++ lavaproj01/resources/views/control.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Route;
+
+class temanController extends Controllers
+{
+        public function index(){
+            $dt = [
+                [
+                'idbuku'=>2,
+                'nama'=>'Steven Bliah',
+                'alamat'=>'Jl Terusan Sulfat No 86A',
+                'kota'=>'Batu',
+                'telp'=>'08129384714',
+                'wa'=>'09782919874',
+                ],
+
+            ];
+            return view('teman',compact('dt') );
+        }
+}
---
+++ lavaproj01/resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<div class="container">
+
+<a href="./">Home</a>
+<a href="./teman">Teman</a>
+
+</div>
+
+
+<div class="container">
+  <h3>Data Teman</h3>
+
+  <table>
+        <caption>Daftar Buku</caption>
+        <thead>
+            <tr>
+                <th>idbuku</th>
+                <th>Nama</th>
+                <th>Alamat</th>
+                <th>Kota</th>
+                <th>Telp</th>
+                <th>WA</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Data akan diisi di sini -->
+        </tbody>
+    </table>
+
+</body>
+</html>
+
+</div>
